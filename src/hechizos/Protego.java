685f5d00e0b9f0c3ba3d667c8228ccce44766957
Franco Ruggieri <101886749+FranRuggi@users.noreply.github.com>
2024-10-30 02:13:20 -0300

    cambio objetivo x lanzador linea en syso

--- src/hechizos/Protego.java
+++ src/hechizos/Protego.java
@@ -1,5 +1,10 @@
 package hechizos;
 
+import org.jpl7.Atom;
+import org.jpl7.Integer;
+import org.jpl7.Query;
+import org.jpl7.Term;
+
 import personajes.Personaje;
 
 public class Protego implements Hechizo {
@@ -7,11 +12,15 @@ public class Protego implements Hechizo {
 
 	@Override
 	public boolean ejecutar(Personaje lanzador, Personaje objetivo) {
-		if (lanzador.getNivelMagia() < COSTO)
-			return false;
+		Query queryConection = new Query("consult",new Term[] {new Atom("MagosVsMortifagos.pl")});
+    	queryConection.hasSolution(); 
+    	
+    	Query queryHechizo = new Query("ejecutarHechizo", new Term[] {new Integer(COSTO), new Integer(lanzador.getNivelMagia())});
+    	if(!queryHechizo.hasSolution())
+    		return false;
 		lanzador.disminuirNivelMagia(COSTO);
 		lanzador.recibirDaño(-20); // Ejemplo de incremento de defensa
-		System.out.println("Un escudo mágico protege a " + objetivo.getNombre());
+		System.out.println("Un escudo mágico protege a " + lanzador.getNombre()); //Cambio en lanzador
 		return true;
 	}
 
