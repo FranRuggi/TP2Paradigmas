d6828c9fa404849d89c838f104aa6bd5d9690943
Franco Ruggieri <101886749+FranRuggi@users.noreply.github.com>
2024-10-30 23:52:19 -0300

    Cambio en la forma de ataque, integracion prolog

--- src/hechizos/Sectumsempra2.java
+++ src/hechizos/Sectumsempra2.java
@@ -9,30 +9,15 @@ public class Sectumsempra2 implements Hechizo {
 	@Override
 	public boolean ejecutar(Personaje lanzador, Personaje objetivo) {
     	
-		
 			if (lanzador.getNivelMagia() < COSTO)
 				return false;
 			lanzador.disminuirNivelMagia(COSTO);
 			objetivo.recibirDaño(50); // Aplica un daño oscuro y crítico
 			return true;
     	
-		/*
-    	if (lanzador.getNivelMagia() < COSTO)
-			return false;
-		lanzador.disminuirNivelMagia(COSTO);
-		objetivo.recibirDaño(50);// Aplica un daño oscuro y crítico
-		return true;
-		*/
 	}
-	/*
-	 * Ideas de prolog para reemplazar logica de verifiacion de lanzamiento de hechizos
-	 * ejecutar(COSTOHECHIZO,MAGIALANZADOR) : -
-	 * 			MAGIALANZADOR >= COSTOHECHIZO.
-	 * hechizo(SECTUMSEMPRA) :-
-	 * 		   			
-	 */
 	
-
+	
 	@Override
 	public String obtenerNombre() {
 		return "Sectumsempra";
--- src/personajes/Batallon.java
+++ src/personajes/Batallon.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import org.jpl7.Atom;
+import org.jpl7.Query;
+import org.jpl7.Term;
+
 import hechizos.Hechizo;
 import hechizos.HechizoFactory;
 
@@ -28,8 +32,34 @@ public class Batallon {
 			Personaje objetivo = otroBatallon.obtenerPersonajeSaludable();
 			if (objetivo != null) {
 				// Seleccionar un hechizo aleatorio
-				Hechizo hechizo = HechizoFactory.crearHechizo(atacante.getTipo()); // Asumiendo que hay un método
-																					// getTipo en Personaje
+				
+				//Creo que seria mejor poner las querys y cosas de prolog en un metodo diferente, tal vez privado que solo lo use
+				//el metodo atacar, simplemente para separar un poco la logica y las responsabilidades de cada metodo
+				
+				// queryConection lo necesitamos para poder consultar a la base de datos de prolog. Es la coneccion
+				Query queryConection = new Query("consult",new Term[] {new Atom("MagosVsMortifagosV2.pl")});
+		    	queryConection.hasSolution();
+		    	
+				// Construimos la consulta para Prolog con nivel de magia y tipo de personaje
+		        																				//Esta parte se podria evitar si directamente escribimos en minuscula los nombres, pero no es tan grave
+				String queryStr = "hechizos_disponibles(" + atacante.getNivelMagia() + ", " + atacante.getTipo().toString().toLowerCase() + ", Hechizos)";
+		        Query queryHechizosDisponibles = new Query(queryStr);
+		        //Con queryHechizosDisponibles, prolog nos devuelve una lista de hechizos disponibles que tiene nuestro personaje en el momento de atacar
+		        List<String> hechizos = new ArrayList<>();
+		        if (queryHechizosDisponibles.hasSolution()) {
+		        	Term hechizosTerm = queryHechizosDisponibles.oneSolution().get("Hechizos");
+		            Term[] soluciones = Term.listToTermArray(hechizosTerm);
+		            
+		            for (Term hechizo : soluciones) {
+		                hechizos.add(hechizo.name());
+		            }
+		        }
+		        //Tenemos la lista preparada para decidir que hechizo crear en base a la disponibilidad del personaje atacante
+		        
+		        //En este momento deberiamos ser capaces de elegir de forma random un hechizo dentro de los disponibles que tiene el atacante
+		        
+		        rand.nextInt((hechizos.size()));
+				Hechizo hechizo = HechizoFactory.crearHechizo(atacante.getTipo());
 				boolean hechizoEjecutado = hechizo.ejecutar(atacante, objetivo); // Ejecutar hechizo
 
 				if (!hechizoEjecutado) {
